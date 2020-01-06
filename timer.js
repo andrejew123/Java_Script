718d41d2f0bf4cdc2919ca6b35ef700b0c07d298
andrejew123 <i.andrejewicz@gmail.com>
2020-01-06 17:39:16 +0100

    Added java scripts projects

---
+++ timer.js
@@ -0,0 +1,17 @@
+function odliczanie()
+	{
+	
+		var dzisiaj = new Date();
+		var dzien = dzisiaj.getDate();
+		var miesiac = dzisiaj.getMonth()+1;
+		var rok = dzisiaj.getFullYear();
+		var godzina = dzisiaj.getHours();
+		if (godzina<10) godzina = "0"+godzina;
+		var minuta = dzisiaj.getMinutes();
+		if (minuta<10) minuta = "0" + minuta;
+		var secunda = dzisiaj.getSeconds();
+		if (secunda<10) secunda = "0" + secunda;
+		document.getElementById("zegar").innerHTML = dzien + "/" + miesiac + "/" + rok + "|" + godzina + ":" + minuta + ":" + secunda;
+
+	setTimeout("odliczanie()", 1000);
+	}
